848a0d84781a625086be303787c86083f66e0bb5
ajuni-adnu <232771789+ajuni-adnu@users.noreply.github.com>
2025-12-08 13:07:32 +0800

    Add which skills to level up

--- main.cpp
+++ main.cpp
@@ -3,14 +3,29 @@ using namespace std;
 
 int intro()
 {
-    int n = 24;
-    cout <<"After surviving Arlong’s invasion of Cocoyashi Village, Keigan “Kei” Lee vows revenge.\n Beaten and left for dead, he spends eight years training in isolation to surpass his limits.";
+    int n = 48;
+    cout <<"After surviving Arlong’s invasion of Cocoyashi Village, Keigan “Kei” Lee vows revenge.\n Beaten and left for dead, he spends eight years training in isolation to surpass his limits.\n";
     while(n != 0){
         cout << n << "\n";
         n--;
+        cout<<"Choose which skill to train:";
+        cout << R"(
+  1. HP, Each session improves durability.
+  2. ATK, Physical strength grows more steadily.
+  3. END, Represents defense/stamina.
+  4. EP (Pool), Expands total energy capacity.
+  5. EP Regen, Determines how much EP returns per turn.
+            )";
     }
+    return n;
+}
+
+int bossFight()
+{
     return 0;
 }
+
+
 
 int main()
 {
