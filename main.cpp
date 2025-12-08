c75935db884e3dd046f727ad9ad706e852ce16e8
ajuni-adnu <232771789+ajuni-adnu@users.noreply.github.com>
2025-12-08 13:08:32 +0800

    Add basic conditional placeholder for skills

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 using namespace std;
 
+
+
 int intro()
 {
-    int n = 48;
+    int n = 0;
+    int choice;
     cout <<"After surviving Arlong’s invasion of Cocoyashi Village, Keigan “Kei” Lee vows revenge.\n Beaten and left for dead, he spends eight years training in isolation to surpass his limits.\n";
-    while(n != 0){
-        cout << n << "\n";
-        n--;
+    while(n != 48){
+        cout << "\nMonth " << n << endl;
+        n++;
         cout<<"Choose which skill to train:";
         cout << R"(
   1. HP, Each session improves durability.
@@ -15,7 +18,20 @@ int intro()
   3. END, Represents defense/stamina.
   4. EP (Pool), Expands total energy capacity.
   5. EP Regen, Determines how much EP returns per turn.
-            )";
+)";
+        cout << "What would you like to train? (1,2,3,4,5): "; cin >> choice;
+        
+        if (choice == 1){
+        
+        } else if (choice == 2){
+            
+        } else if (choice == 3){
+            
+        } else if (choice == 4){
+            
+        } else if (choice == 5){
+            
+        }
     }
     return n;
 }
